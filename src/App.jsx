f51e5c69851823a57fdc23abf5eefe873988ce68
MiguelDN <mpsdiaz@gmail.com>
2024-03-20 12:26:29 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload y he cambiado en app.jsx.
         </p>
         <a
           className="App-link"
