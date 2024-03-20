afb7d90336e2a65e4fe502818f598cc5f978ec18
MiguelDN <mpsdiaz@gmail.com>
2024-03-20 16:09:43 +0100

    fetch items y pass

--- src/context/DataProvider.jsx
+++ src/context/DataProvider.jsx
@@ -40,17 +40,16 @@
 
 // export default DataProvider;
 
-
-
 import React, { useState, useEffect } from "react";
 import DataContext from "./DataContext";
 
 const DataProvider = ({ children }) => {
-  const [data, setData] = useState(null);
+  const [items, setItems] = useState(null);
+  const [pass, setPass] = useState(null);
   const [loading, setLoading] = useState(true); // Nuevo estado para manejar la carga
 
-  const url = 
-  "https://getmiztondata.azurewebsites.net/api/getMondayData?code=f9u-Sq2-C-_hooTqZn8xP2XxvQd1oKPeg-Y0xyX76BKxAzFuwSkTZA==";
+  const url =
+    "https://getmiztondata.azurewebsites.net/api/getMondayData?code=f9u-Sq2-C-_hooTqZn8xP2XxvQd1oKPeg-Y0xyX76BKxAzFuwSkTZA==";
 
   const fetchItems = async () => {
     setLoading(true); // Inicia la carga
@@ -59,15 +58,40 @@ const DataProvider = ({ children }) => {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          'get_items': "query{boards(ids: 6097786561) {items_page(limit: 25){cursor items{id name} }}}"
+          get_items:
+            "query{boards(ids: 6097786561) {items_page(limit: 25){cursor items{id name} }}}",
         }),
       });
 
       if (!response.ok) throw new Error("La petición falló");
-      console.log('contentType',)
+      console.log("contentType");
       console.log("response:", response);
       const data = await response.json();
-      setData(data.data.boards[0].items_page.items); // Actualiza los datos
+      setItems(data.data.boards[0].items_page.items); // Actualiza los datos
+      setLoading(false); // Finaliza la carga
+    } catch (error) {
+      console.error("Error en la petición:", error);
+      // setLoading(false); // Asegúrate de manejar el estado de carga incluso en caso de error
+    }
+  };
+
+
+  const fetchpass = async () => {
+    setLoading(true); // Inicia la carga
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          get_pass:"get_pass",
+        }),
+      });
+
+      if (!response.ok) throw new Error("La petición falló");
+      console.log("contentType");
+      console.log("response:", response);
+      const data = await response.json();
+      setPass(data); // Actualiza los datos
       setLoading(false); // Finaliza la carga
     } catch (error) {
       console.error("Error en la petición:", error);
@@ -77,13 +101,14 @@ const DataProvider = ({ children }) => {
 
   useEffect(() => {
     fetchItems();
+    fetchpass(); 
   }, []);
 
   if (loading) {
     return <div>Cargando datos...</div>; // Puedes personalizar este componente de carga como prefieras
   }
 
-  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
+  return <DataContext.Provider value={items}>{children}</DataContext.Provider>;
 };
 
 export default DataProvider;
