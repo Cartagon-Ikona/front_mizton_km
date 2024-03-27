35bc8e5083ae8d22cb6d071ff006345e8b473222
MiguelDN <mpsdiaz@gmail.com>
2024-03-27 18:28:58 +0100

    .

--- src/components/PassText.jsx
+++ src/components/PassText.jsx
@@ -7,7 +7,7 @@ export default function FormPropsTextFields({ password, onPasswordChange, handle
   const handleSubmit = (event) => {
     event.preventDefault();
     
-   log console.log("handlsubmit")
+    console.log("handlsubmit")
     handleSend()
   };
 
